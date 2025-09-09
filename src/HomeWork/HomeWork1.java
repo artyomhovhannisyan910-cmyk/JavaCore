634ad0e69ab28afbef31d666e15a2ea84844ab55
Artyom Hovhannisyan <artyomhovhannisyan910@gmail.com>
2025-09-09 19:31:22 +0400

    HomeWork1

---
+++ src/HomeWork/HomeWork1.java
@@ -0,0 +1,17 @@
+package HomeWork;
+
+import java.sql.SQLOutput;
+
+public class HomeWork1 {
+    public static void main(String[] args) {
+        int x, y;
+        {
+            System.out.println('x');
+        }
+        x = 20;
+        y = 10;
+        if (x > y) ;
+        System.out.println("X-ը մեծ է");
+
+    }
+}
---
+++ src/HomeWork/HomeWork2.java
@@ -0,0 +1,9 @@
+package HomeWork;
+
+public class HomeWork2 {
+    public static void main(String[] args) {
+        int x;
+        for (  x = 1; x <= 5; x++)
+            System.out.println("x"  + x);
+    }
+}
