868689177bdc018e5527f356d13f893413d650f0
Artyom Hovhannisyan <artyomhovhannisyan910@gmail.com>
2025-09-09 21:59:01 +0400

    HomeWork1 ուղղված

--- src/HomeWork/HomeWork1.java
+++ src/HomeWork/HomeWork1.java
@@ -4,14 +4,15 @@ import java.sql.SQLOutput;
 
 public class HomeWork1 {
     public static void main(String[] args) {
-        int x, y;
-        {
-            System.out.println('x');
-        }
-        x = 20;
-        y = 10;
-        if (x > y) ;
-        System.out.println("X-ը մեծ է");
+        int x = 5;
+        int y = 3;
 
+        if (x > y) {
+            System.out.println(x);
+        } else if (y > x) {
+            System.out.println(y);
+        } else {
+            System.out.println(x==y);
+        }
     }
 }
