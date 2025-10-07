e1cdb7c8aa180066c99847a7b5c46a833937a11d
Artyom Hovhannisyan <artyomhovhannisyan910@gmail.com>
2025-10-08 00:48:40 +0400

    DynamicArray Home-Work2

--- src/DynamicArray/DynamicArray.java
+++ src/DynamicArray/DynamicArray.java
@@ -3,6 +3,7 @@ package DynamicArray;
 public class DynamicArray {
     private int[] array = new int[10];
     private int size = 0;
+
     void extend() {
         int[] newArray = new int[array.length + 10];
         for (int i = 0; i < array.length; i++) {
@@ -33,5 +34,56 @@ public class DynamicArray {
             System.out.println(array[i] + " ");
         }
         System.out.println();
+    }
+
+    public void deleteByIndex(int index) {
+        if (index < 0 || index >= size)
+            System.out.println("Նման ինդեքսով թիվ չկա");
+
+        for (int i = index; i < size - 1; i++) {
+
+            array[i] = array[i + 1];
+
+        }
+        size--;
+
+    }
+
+    public void set(int index, int value) {
+        if (index < 0 || index >= size) {
+            System.out.println("Նման ինդեքսով թիվ չկա");
+
+        }
+        array[index] = value;
+    }
+
+    public void add(int index, int value) {
+        if (index < 0 || index > size)
+            System.out.println("Նման ինդեքսով թիվ չկա");
+        if (size == array.length)
+            extend();
+        for (int i = size; i > index; i--) {
+            array[i] = array[i - 1];
+
+        }
+        array[index] = value;
+        size++;
+    }
+
+    public boolean exists(int value) {
+        for (int i = 0; i < size; i++) {
+            if (array[i] == value)
+                return true;
+
+        }
+        return false;
+    }
+
+    public int getIndexByValue(int value) {
+        for (int i = 0; i < size; i++) {
+            if (array[i] == value)
+                return i;
+        }
+        return -1;
     }
 }
--- src/DynamicArray/DynamicArrayDemo.java
+++ src/DynamicArray/DynamicArrayDemo.java
@@ -9,11 +9,19 @@ public class DynamicArrayDemo {
         array.add(6);
         array.add(8);
         array.add(10);
-
         System.out.println("new elements");
         array.print();
 
         System.out.println(" 6 = " + array.GetByIndex(2)); // index=2 → 6
-        System.out.println(" 10 = " + array.GetByIndex(4)); // index=4 → 10
+        System.out.println(" 10 = " + array.GetByIndex(4)); // index
+        array.set(1, 44);
+        array.print();
+        array.add(2, 22);
+        array.print();
+        array.deleteByIndex(1);
+        System.out.println("100 ? " + array.exists(100));
+        System.out.println("22 ? " + array.exists(22));
+        System.out.println("44 index = " + array.getIndexByValue(44));
+        System.out.println("157 index = " + array.getIndexByValue(157));
     }
 }
